6244d142b29ec139600f950cdb442d10f4c804bc
Nikita <nicstyle22@gmail.com>
2022-10-30 13:40:32 +0300

    add local-storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,33 @@
-import { configureStore} from '@reduxjs/toolkit'
-import { todoReducer} from './todo-slice'
+import { configureStore } from '@reduxjs/toolkit';
+import { todoReducer } from './todo-slice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 export const store = configureStore({
-reducer:todoReducer
-})
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
+export const persistor = persistStore(store);
